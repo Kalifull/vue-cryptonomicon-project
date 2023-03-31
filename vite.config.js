8892a8e68a6c9cc65ffc1dc9a0c768942069629b
Vorona Aleksey <aleksey.vorona@nordclan.com>
2023-03-31 18:45:51 +0300

    init commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+import eslintPlugin from 'vite-plugin-eslint';
+
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [vue(), eslintPlugin()],
+
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
